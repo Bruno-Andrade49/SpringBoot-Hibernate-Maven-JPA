8d6b36c8df4a075d0a494267cf12122f188bc58b
Bruno-Andrade49 <b.emanueandrade@hotmail.com>
2021-07-12 11:15:23 -0300

    Resivando OrderStatus

--- src/main/java/com/example/projeto/entities/Ordenacao.java
+++ src/main/java/com/example/projeto/entities/Ordenacao.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 
 import org.springframework.stereotype.Repository;
 
+import com.example.springboot.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
@@ -27,20 +28,31 @@ public class Ordenacao implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	
+	private Integer OrderStatus;
 
 	
+	public Integer getOrderStatus() {
+		return OrderStatus;
+	}
+
+	public void setOrderStatus(Integer orderStatus) {
+		OrderStatus = orderStatus;
+	}
+
+	public Ordenacao(Integer id, Instant moment, Integer orderStatus, Usuario userClient) {
+		this.id = id;
+		this.moment = moment;
+		this.OrderStatus = orderStatus;
+		this.userClient = userClient;
+	}
+
 	@ManyToOne
 	@JoinColumn(name = "Client_ID")
 	private Usuario userClient;
 
 	public Ordenacao() {
 
-	}
-
-	public Ordenacao(Integer id, Instant moment, Usuario client) {
-		this.id = id;
-		this.moment = moment;
-		this.userClient = client;
 	}
 
 	public Integer getId() {
@@ -71,9 +83,10 @@ public class Ordenacao implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((userClient == null) ? 0 : userClient.hashCode());
+		result = prime * result + ((OrderStatus == null) ? 0 : OrderStatus.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((moment == null) ? 0 : moment.hashCode());
+		result = prime * result + ((userClient == null) ? 0 : userClient.hashCode());
 		return result;
 	}
 
@@ -86,10 +99,10 @@ public class Ordenacao implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Ordenacao other = (Ordenacao) obj;
-		if (userClient == null) {
-			if (other.userClient != null)
+		if (OrderStatus == null) {
+			if (other.OrderStatus != null)
 				return false;
-		} else if (!userClient.equals(other.userClient))
+		} else if (!OrderStatus.equals(other.OrderStatus))
 			return false;
 		if (id == null) {
 			if (other.id != null)
@@ -100,6 +113,11 @@ public class Ordenacao implements Serializable {
 			if (other.moment != null)
 				return false;
 		} else if (!moment.equals(other.moment))
+			return false;
+		if (userClient == null) {
+			if (other.userClient != null)
+				return false;
+		} else if (!userClient.equals(other.userClient))
 			return false;
 		return true;
 	}
--- src/main/java/com/example/springboot/enums/OrderStatus.java
+++ src/main/java/com/example/springboot/enums/OrderStatus.java
@@ -2,11 +2,28 @@ package com.example.springboot.enums;
 
 public enum OrderStatus {
 	
-	WAITING_PAYMENT,
-	PAID,
-	SHIPPED,
-	DELIVERED,
-	CANCELED;
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
 	
-
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus valores : OrderStatus.values()) {
+			if (valores.getCode() == code) {
+				return valores;
+			} 
+		}
+			throw new IllegalArgumentException("Nada foi encontrado, código inválido");
+	}
 }
